484ea01f333bc42f81fc001721e7098fbc665f06
timothychou <tchou@caltech.edu>
2015-10-25 16:52:03 -0700

    Refactoring of networkObject class
    processEvent is now defined in networkObject, with all subclasses overriding
    private functions called by processEvent depending on the type of event

--- source/networkobject.py
+++ source/networkobject.py
@@ -6,14 +6,28 @@ from packet import Packet
 class NetworkObject(object):
     """abstract class for all network objects"""
 
-    def processEvent(self, events):
-        """Processes events
 
-        Input: list of events from the event processor
-        Output: list of new events to enqueu in event processor"""
+    def processEvent(self, event):
+        """ Processes one event, depending on what the event is.
 
-        raise NotImplementedError('subclasses should override processEvent')
+        This method should be called directly or indirectly by an EventHandler.
 
+        :param event: Event to be processed.
+        :return: list of new Events to be enqueued.
+        """
+        if isinstance(event, PacketEvent):
+            return self._processPacketEvent(event)
+        elif isintance(event, Event):
+            return self._processOtherEvent(event)
+        else:
+            raise AssertionError('process event should only be given an event')
+
+
+    def _processPacketEvent(self, packet_event):
+        raise NotImplementedError('This should be overriden by subclass')
+
+    def _processOtherEvent(self, event):
+        raise NotImplementedError('This should be overriden by subclass')
 
 class Link(NetworkObject):
     """Represents link in a network
@@ -27,37 +41,30 @@ class Link(NetworkObject):
         self.latency = latency
         self.speed = speed
 
-    def processEvent(self, event):
-        """ Processes one event.
-
-        Links should receive ONLY PacketEvents. Any other Event passed will raise an AssertionError.
-
-        :param event: Event to be processed.
-        :return: list of new Events to be enqueued.
-        """
-        assert type(event) is PacketEvent
-
-        newEvents = []
-        newtimestamp = event.timestamp + self.latency
-        return self._processPacketEvent(event, newtimestamp)
-
-    def _processPacketEvent(self, packet_event, newtimestamp):
-        """Processes packet events.
+    def _processPacketEvent(self, packet_event):
+        """Processes packet events
 
         Creates a new PacketEvent of the host receiving the packet at some time in the future.
         """
-        return PacketEvent(newtimestamp,
+        return [PacketEvent(packet_event.timestamp + self.latency,
                            self, self._otherNode(packet_event.sender),
                            packet_event.packet,
-                           packet_event.logMessage)
+                           packet_event.logMessage)]
+
+    def _processOtherEvent(self, event):
+        """ Processes non-packet events
+
+        Links should only get packet events, so simply raises an error
+        """
+        raise AssertionError('Links should receive only packet events')
 
     def _otherNode(self, node):
         """returns the other node the link is connected to"""
-        return self.nodeB if node == self.nodeA else self.nodeB
+        return self.nodeB if node == self.nodeA else self.nodeA
 
 
 class Node(NetworkObject):
-    """ Abstract class that represents a node in a network
+    """ class that represents a node in a network
 
     This class represents a node in a network connected by edges"""
 
@@ -84,32 +91,18 @@ class Host(Node):
         super(self.__class__, self).__init__(address, links)
         assert len(links) == 1
 
-    def processEvent(self, event):
-        """ Processes one event, depending on what the event is.
-
-        This method should be called directly or indirectly by an EventHandler.
-
-        :param event: Event to be processed.
-        :return: list of new Events to be enqueued.
-        """
-        if type(event) is PacketEvent:
-            return self._processPacketEvent(event)
-        elif type(event) is Event:
-            # Generic Event, implementation not handled yet.
-            raise NotImplementedError('Host not able to handle generic Event yet')
-        else:
-            # Not an Event...
-            pass
 
     def _processPacketEvent(self, event):
         """Processes packet event
 
         sends back an ack as needed"""
         packet = event.packet
-        # TODO (choutim) Verify that this Host is the proper recipient of the packet (use packet.dest)
+        assert packet.dest = self.address
         if not packet.ack and not packet.corrupted:
             newPacket = Packet(packet.dest, packet.source, ack=True)
-            return PacketEvent(event.timestamp, self, self.links[0], newPacket)
+            return [PacketEvent(event.timestamp, self,
+                                self.links[0], newPacket)]
+        return []
 
     def sendPackets(self, numPackets, dest, timestamp):
         """Send numPackets packets to destination dest.
@@ -117,8 +110,9 @@ class Host(Node):
         This method should be called directly or indirectly by an EventHandler.
         """
         events = []
-        for i in xrange(len(numPackets)):
+        for i in xrange(numPackets):
             packet = Packet(self.address, dest, i)
+            # TODO make it so packets are not all sent at the same time
             events.append(PacketEvent(timestamp, self, self.links[0], packet))
         return events
 
@@ -138,32 +132,22 @@ class Router(NetworkObject):
         # _UpdateRoutingTable should be called once. Otherwise, the Router
         # will not be able to forward anything at all.
 
-    def processEvent(self, event):
-        """ Processes one event, depending on what the event is.
-
-        This method should be called directly or indirectly by an EventHandler.
-
-        :param event: Event to be processed.
-        :return: list of new Events to be enqueued.
-        """
-        newEvents = []
-        if type(event) is PacketEvent:
-            newEvents.append(self._processPacketEvent(event))
-        return newEvents
 
     def _processPacketEvent(self, event):
         """Processes packet event.
 
-        Timestamp is not changed because there is no latency through the router.
+        Timestamp is not changed because there is no
+        latency through the router.
         """
-        return PacketEvent(event.timestamp, self,
+        return [PacketEvent(event.timestamp, self,
                            self.getRoute(event.packet.destination),
-                           event.packet)
+                           event.packet)]
 
     def _UpdateRoutingTable(self):
         """ Updates the internal routing table.
 
-        This method should be the result of an Event that tells informs the Router to update.
+        This method should be the result of an Event that informs 
+        the Router to update.
         """
         # TODO(choutim) Implement at a later time.
         pass
