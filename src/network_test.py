11e7372ef51ce6a916350ef27aef13179e04865d
tongcharlie <tongcharlie@gmail.com>
2015-11-04 05:18:02 -0800

    Change network_test to standardized format

--- src/network_test.py
+++ src/network_test.py
@@ -6,7 +6,7 @@ from network import *
 
 class NetworkTest(unittest.TestCase):
 
-    def testcreate(self):
+    def testCreate(self):
         network1 = Network()
         a = network1.addHost()
         b = network1.addRouter()
@@ -15,19 +15,19 @@ class NetworkTest(unittest.TestCase):
         network1.addLink(b, c, rate=700, latency=1000)
         network1.addEvent(a, c, 1000000, 1)
 
-        self.failUnlessEqual(len(network1.events), 1)
-        self.failUnlessEqual(len(network1.events), 1)
+        self.assertEqual(len(network1.events), 1)
+        self.assertEqual(len(network1.events), 1)
 
-    def testimport(self):
+    def testImport(self):
         # Generate data file
         filename = 'testfile.json'
         network1 = Network()
         a = network1.addHost()
         b = network1.addRouter()
         c = network1.addHost()
-        self.failUnlessEqual(a, 0)
-        self.failUnlessEqual(b, 1)
-        self.failUnlessEqual(c, 2)
+        self.assertEqual(a, 0)
+        self.assertEqual(b, 1)
+        self.assertEqual(c, 2)
         network1.addLink(a, b, rate=4000, latency=3)
         network1.addLink(b, c, rate=700, latency=1000)
         network1.addEvent(a, c, 1000000, 1)
@@ -37,24 +37,22 @@ class NetworkTest(unittest.TestCase):
         network1.save(filename)
         network2 = Network()
         network2.load(filename)
-        assert('events' in network2.G.graph)
-        assert(len(network2.events) == 1)
+        self.assertIn('events', network2.G.graph)
+        self.assertEqual(len(network2.events), 1)
 
-    def testgets(self):
+    def testGets(self):
         N = Network()
-        self.failUnlessEqual(N.getNewNodeId(), 0)
-        self.failUnlessEqual(N.getNewNodeId(), 0)
+        self.assertEqual(N.getNewNodeId(), 0)
+        self.assertEqual(N.getNewNodeId(), 0)
         a = N.addHost()
         b = N.addRouter()
         c = N.addHost()
-        self.failUnlessEqual(N.getNewNodeId(), 3)
+        self.assertEqual(N.getNewNodeId(), 3)
         N.addLink(a, b, rate=4000, latency=3)
         N.addLink(b, c, rate=700, latency=1000)
         N.addEvent(a, c, 1000000, 1)
-        self.failUnlessEqual(len(N.getNodeList()), 3)
-        self.failUnlessEqual(len(N.getLinkList()), 2)
-
-
+        self.assertEqual(len(N.getNodeList()), 3)
+        self.assertEqual(len(N.getLinkList()), 2)
 
 
 if __name__ == '__main__':
